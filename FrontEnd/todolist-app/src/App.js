b7e20c916286b6f3fad83e85a9635183a941ad2e
sergio <sergio19101992@gmail.com>
2021-05-31 14:18:23 -0300

    Implementando checkbox en tabla

--- FrontEnd/todolist-app/src/App.js
+++ FrontEnd/todolist-app/src/App.js
@@ -3,7 +3,6 @@ import React, { Component } from 'react';
 import { ItemService } from './service/ItemService';
 import { DataTable } from 'primereact/datatable'
 import { Column } from 'primereact/components/column/Column';
-import {Checkbox} from 'primereact/checkbox';
  
 import 'primereact/resources/themes/nova-alt/theme.css';
 import 'primereact/resources/primereact.min.css';
@@ -18,15 +17,28 @@ export default class App extends Component{
     constructor(){
       super();
       this.state = {
+        selectedItem : null,
         item:{
         id: null,
         name: null,
-        complete: false}
+        complete: false},
       };
       this.itemService = new ItemService();
       this.save = this.save.bind(this);
-    }
+      this.onRowSelect = this.onRowSelect.bind(this);
+      this.onRowUnselect = this.onRowUnselect.bind(this);
 
+      
+    }
+    
+
+    onRowSelect(event) {
+      this.toast.show({ severity: 'info', summary: 'Product Selected', detail: `Name: ${event.data.name}`, life: 3000 });
+  }
+
+  onRowUnselect(event) {
+      this.toast.show({ severity: 'warn', summary: 'Product Unselected', detail: `Name: ${event.data.name}`, life: 3000 });
+  }
     componentDidMount(){
       this.itemService.getAll().then(data => this.setState({items: data}))
     }
@@ -36,21 +48,34 @@ export default class App extends Component{
       this.itemService.save(this.state.item).then(data => {
       })
       this.itemService.getAll().then(data => this.setState({items: data}))
-      document.getElementById('item-imput').reset();
     }
 
+    editProduct(product) {
+      this.setState({
+          product: { ...product },
+          productDialog: true
+      });
+  }
+    actionBodyTemplate(rowData) {
+      return (
+          <React.Fragment>
+              <Button icon="pi pi-pencil" className="p-button-rounded p-button-success p-mr-2" onClick={() => this.editProduct(rowData)} />
+          </React.Fragment>
+      );
+  }
     render(){
       return (
         <Panel header="ToDo List">
-          <DataTable value={this.state.items}> 
-            <Column field="name" header="Task"></Column>
-          </DataTable>
-
+                    <DataTable value={this.state.items} selectionMode="checkbox" selection={this.state.selectedItem} onSelectionChange={(e) => this.setState({ selectedItem: e.value })} dataKey="id">                      
+                      <Column selectionMode="multiple" headerStyle={{width: '3em'}}></Column>
+                      <Column field="name" header="Task"></Column>
+                      <Column body={this.actionBodyTemplate}></Column>
+                    </DataTable>
           <br/>
           <br/>
           <form id="item-imput">
             <span className="p-float-label">
-                <InputText  style={{width : '20%'}} id="name" onChange={(e) => {
+                <InputText id="name" onChange={(e) => {
                 let val = e.target.value;
                 this.setState(prevState =>{
                   let item = Object.assign({}, prevState.item);
