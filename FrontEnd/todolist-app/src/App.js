4bb07794c25302f7dc8fcc908f2748a0caba072d
sergio <sergio19101992@gmail.com>
2021-05-31 01:30:47 -0300

    se muestra los elementos de la baase de datos, se puede crear nuevas tareas. Falta implementar el checkbox y la edicion de la tarea

--- FrontEnd/todolist-app/src/App.js
+++ FrontEnd/todolist-app/src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import { ItemService } from './service/ItemService';
+import { DataTable } from 'primereact/datatable'
+import { Column } from 'primereact/components/column/Column';
+import {Checkbox} from 'primereact/checkbox';
+ 
+import 'primereact/resources/themes/nova-alt/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import { Panel } from 'primereact/panel';
+import { Button } from 'primereact/button';
+import { InputText } from 'primereact/inputtext';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+
+export default class App extends Component{
+    constructor(){
+      super();
+      this.state = {
+        item:{
+        id: null,
+        name: null,
+        complete: false}
+      };
+      this.itemService = new ItemService();
+      this.save = this.save.bind(this);
+    }
+
+    componentDidMount(){
+      this.itemService.getAll().then(data => this.setState({items: data}))
+    }
+  
+    save(){
+      console.log(this.state.item);
+      this.itemService.save(this.state.item).then(data => {
+      })
+      this.itemService.getAll().then(data => this.setState({items: data}))
+      document.getElementById('item-imput').reset();
+    }
+
+    render(){
+      return (
+        <Panel header="ToDo List">
+          <DataTable value={this.state.items}> 
+            <Column field="name" header="Task"></Column>
+          </DataTable>
+
+          <br/>
+          <br/>
+          <form id="item-imput">
+            <span className="p-float-label">
+                <InputText  style={{width : '20%'}} id="name" onChange={(e) => {
+                let val = e.target.value;
+                this.setState(prevState =>{
+                  let item = Object.assign({}, prevState.item);
+                  item.name = val;
+                  console.log(prevState);
+                  return {item};
+                })}
+                } />    
+                <label htmlFor="in">New Task</label>
+            </span>
+          </form>
+          <br></br>
+          <Button label="Add" onClick={this.save}></Button>
+        </Panel>
+      );
+    }
+  }
---
+++ FrontEnd/todolist-app/src/service/ItemService.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+export class ItemService{
+        baseUrl = "http://localhost:8080/api"
+    
+        getAll(){
+            return axios.get(this.baseUrl + "/all").then(res => res.data);
+        }
+
+        save(item){
+            return axios.post(this.baseUrl + "/save",item).then(res => res.data);
+        }
+
+        
+}
